942cff17fcb4b600bd22b38fdf84ba23320ddfa3
Ishan Patel <ip5038@gmail.com>
2022-11-05 11:36:09 -0400

    added nav graph

--- app/src/main/java/com/mobileapp/hw06/GameBoardFragment.java
+++ app/src/main/java/com/mobileapp/hw06/GameBoardFragment.java
@@ -7,13 +7,63 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class GameBoardFragment extends Fragment {
+import com.mobileapp.hw06.databinding.FragmentGameBoardBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameBoardFragment extends Fragment implements View.OnClickListener {
+    private FragmentGameBoardBinding binding;
+    private Button newGameButton;
+    private List<List<Button>> gameBoard;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentGameBoardBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        newGameButton = binding.newGameButton;
+        setUpGameBoard();
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_board, container, false);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        String btnId = getResources().getResourceEntryName(v.getId());
+        int btnIdLength = btnId.length();
+        int row = btnId.charAt(btnIdLength - 2) - 48;
+        int col = btnId.charAt(btnIdLength - 1) - 48;
+        
+    }
+
+    private void setUpGameBoard()
+    {
+        List<Button> row0 = new ArrayList<Button>();
+        List<Button> row1 = new ArrayList<Button>();
+        List<Button> row2 = new ArrayList<Button>();
+        gameBoard = new ArrayList<List<Button>>();
+        row0.add(binding.light00);
+        row0.add(binding.light01);
+        row0.add(binding.light02);
+        row1.add(binding.light10);
+        row1.add(binding.light11);
+        row1.add(binding.light12);
+        row2.add(binding.light20);
+        row2.add(binding.light21);
+        row2.add(binding.light22);
+        gameBoard.add(row0);
+        gameBoard.add(row1);
+        gameBoard.add(row2);
+
+        for(List<Button> row : gameBoard) {
+            for(Button b : row) {
+                b.setOnClickListener(this);
+            }
+        }
+
     }
 }
