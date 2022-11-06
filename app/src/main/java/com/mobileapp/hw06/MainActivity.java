89e0a1d8499de7d680b80af3fcf7d145a99f60fc
Ishan Patel <ip5038@gmail.com>
2022-11-05 23:40:31 -0400

    finish hw06

--- app/src/main/java/com/mobileapp/hw06/MainActivity.java
+++ app/src/main/java/com/mobileapp/hw06/MainActivity.java
@@ -15,6 +15,15 @@ import android.view.MenuItem;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+/***********************************************
+ Author: Ishan Patel
+ Date: Nov 5 2022
+ Purpose: Crate Lights Out game
+ What Learned: How to create multiple screen app and pass data between each screen.
+ Sources of Help: Stackoverflow and previous projects
+ Time Spent: 6 hours
+ ***********************************************/
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -51,3 +60,24 @@ public class MainActivity extends AppCompatActivity {
                 navController) || super.onOptionsItemSelected(item);
     }
 }
+
+/*
+Mobile App Development I -- COMP.4630 Honor Statement
+The practice of good ethical behavior is essential for
+maintaining good order in the classroom, providing an enriching
+learning experience for students, and training as a practicing
+computing professional upon graduation. This practice is
+manifested in the University's Academic Integrity policy.
+Students are expected to strictly avoid academic dishonesty and
+adhere to the Academic Integrity policy as outlined in the course
+catalog. Violations will be dealt with as outlined therein. All
+programming assignments in this class are to be done by the
+student alone unless otherwise specified. No outside help is
+permitted except the instructor and approved tutors.
+I certify that the work submitted with this assignment is mine
+and was generated in a manner consistent with this document, the
+course academic policy on the course website on Blackboard, and
+the UMass Lowell academic code.
+Date: Nov 5 2022
+Name: Ishan Patel
+*/
